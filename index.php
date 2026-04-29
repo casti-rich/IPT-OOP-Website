0530014667c27ec75830012fab179b8eba5adc35
casti-rich <aldrichsaml@gmail.com>
2026-04-30 00:11:55 +0800

    added front end

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>STORE</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="CSS/products-navbar.css">
+    <link rel="stylesheet" href="CSS/index.css">
 </head>
+
 <body>
     <?php include 'products-navbar.php'; ?>
     <div class=main-content>
+        <div class="container">
+            <div class="carousel">
+                <div class="slides">
+                    <img src="assets/Carousel/banner1.jpg">
+                    <img src="assets/Carousel/banner4.jpg">
+                    <img src="assets/Carousel/banner5.jpg">
+                </div>
+            </div>
+            <div class="products">
+                <div class="product_display1">
+                    <img src="assets/products/ikuyo_kita_model.jpg">
+                </div>
+                <div class="product_display2">
+
+
+                </div>
+            </div>
+
+
+        </div>
 
     </div>
 </body>
+
 </html>
